345d6d38944f87a048eba83b1fb3d8625523bdf5
Vizoee <193793023+Vizoee@users.noreply.github.com>
2025-11-02 21:23:30 +0100

    fix :3

---
+++ dev_vizoee/cc/listener.lua
@@ -0,0 +1,46 @@
+local focalPort = peripheral.find("focal_port")
+
+local function readFocalPort()
+    return focalPort.readIota()
+end
+
+local function hash(str)
+    return textutils.serialise(str)
+end
+
+local function hexget(path)
+    local url = path
+
+    local github = require("github")
+    spell_url = github.convert_url(url)
+    local res = github.api_response(spell_url).content
+    if not res then
+        print("Error")
+        return
+    end
+
+    local hexlator = require("hexlator")
+    local debug = false
+    local stripped = false
+    local verbose = false
+    local compiled = hexlator.compile(res, stripped, verbose, debug)
+
+    hexlator.writeToFocus(compiled)
+end
+
+
+local previous_value = hash(readFocalPort())
+
+
+while true do
+    local val = readFocalPort()
+    if previous_value ~= hash(val) then
+        previous_value = hash(val)
+        if val[1] == "hexget" then
+            print("Processing request: " .. val[2])
+            hexget(val[2])
+            print("Completed")
+        end
+    end
+    sleep(0.1)
+end
